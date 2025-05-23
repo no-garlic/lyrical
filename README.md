e2819d6e31bcfd6523d6763c760e71dff1ead345
Michael Petrou <mpetrou74@hotmail.com>
2025-05-23 14:16:12 +0930

    Refactor song card functionality: move edit/delete button logic to c_card_song.js, streamline event handling, and improve hover behavior for better user experience

--- README.md
+++ README.md
@@ -1,39 +1,40 @@
 # lyrical
 
-
-# Backlog
-B - Validation for song name editing
-B - Stop drag and drop when editing
-B - When editing, hide all other edit and delete buttons when hovering
-B - Drag and drop not showing custom cursors
-B Hide girl in media query for short page
-
 ## Done
-X - Create README with TODO list
-X - Only show edit & delete buttons when hovering the card
-X - Bug with floating circle from top left corner, likely in my drag-drop code
-X Rename MANAGE to NEW
-X Look at fonts and font sizes for the panel titles
-X Set cursor to the end of the field when setting focus, or select all
-X Look at changing Temperature and Max Tokens to orange or blue
-X Look at changing the timeline to orange
-X Show the selected LLM cost per 1M tokens on the sidebar
-X Remove SONG TITLE on names page, put MANAGE SONG NAMES in middle of the page or something
+ - Create README with TODO list
+ - Only show edit & delete buttons when hovering the card
+ - Bug with floating circle from top left corner, likely in my drag-drop code
+ - Rename MANAGE to NEW
+ - Look at fonts and font sizes for the panel titles
+ - Set cursor to the end of the field when setting focus, or select all
+ - Look at changing Temperature and Max Tokens to orange or blue
+ - Look at changing the timeline to orange
+ - Show the selected LLM cost per 1M tokens on the sidebar
+ - Remove SONG TITLE on names page, put MANAGE SONG NAMES in middle of the page or something
+ - Rename util_streamhelper to util_stream_helper
 
-# Todo
-- Refactor song-card javascript out of names.js into util_card_song, so it can be reused
-- Rename util_streamhelper to util_stream_helper
-- Save song status when drag-dropping
-- Able to create a song name disabling the edit / delete behavior
-- Generate Song Names button -> make it work with parameters
-- Message history when calling the LLM for Generate Names
+## Todo
+ - Save song status when drag-dropping
+ - Refactor song-card javascript out of names.js into util_card_song, so it can be reused
+ - Able to create a song name disabling the edit / delete behavior
+
+ - Generate Song Names button -> make it work with parameters
+ - Message history when calling the LLM for Generate Names
 
 
+## Next
+ - API call and update UI for showing the current LLM details, and changing to a different LLM
+ - API call and update UI for showing LLM parameters (temperature and nax tokens)
+ - Refactor js code, tidy up, rename apis, etc
+ - Panel Slider is not implemented
+ - Save Column Widths
+ - Simple filter in LIKED and DISLIKED headers
+ - Login Required for all pages
 
-- API call and update UI for showing the current LLM details, and changing to a different LLM
-- API call and update UI for showing LLM parameters (temperature and nax tokens)
-- Refactor js code, tidy up, rename apis, etc
-- Panel Slider is not implemented
-- Save Column Widths
-- Simple filter in LIKED and DISLIKED headers
-- Login Required for all pages
+
+## Backlog
+ - Validation for song name editing
+ - Stop drag and drop when editing
+ - When editing, hide all other edit and delete buttons when hovering
+ - Drag and drop not showing custom cursors
+ - Hide girl in media query for short page
--- lyrical/static/lyrical/script/page_names.js
+++ lyrical/static/lyrical/script/page_names.js
@@ -1,5 +1,4 @@
-import { apiSongDelete } from './api_song_delete.js';
-import { apiSongEdit } from './api_song_edit.js';
+import { initSongCards } from './c_card_song.js'
 import { makeVerticallyResizable } from './util_sliders_vertical.js'
 import { makeHorizontallyResizable } from './util_sliders_horizontal.js'
 import { DragDropSystem } from './util_dragdrop.js';
@@ -13,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('btn-generate-song-names').onclick = generateSongNames;
 
     // Register the song cards
-    registerSongCards();
+    initSongCards();
 
     // Initialize Drag and Drop
     initDragDropSystem();
@@ -123,120 +122,4 @@ function registerCardForDragDrop(card, dragDropSystem) {
     dragDropSystem.registerDraggable(card, { songId, songName, originalZone });
 }
 
-/*
- * Register the song cards' edit and delete buttons
- */
-function registerSongCards() {
-    // Bind the click events for the song edit and delete buttons
-    Array.from(document.getElementsByClassName('btn-song-edit')).forEach(element => {
-        element.onclick = (event) => { songEditButtonClick(element) };
-    });
-    Array.from(document.getElementsByClassName('btn-song-delete')).forEach(element => {
-        element.onclick = (event) => { songDeleteButtonClick(element) };
-    });
-    
-    // Bind the hover events for the song cards
-    document.querySelectorAll('.song-card').forEach(card => {
-        // Get the song ID from the card
-        const songId = card.dataset.songId;
 
-        // Show the song card hover elements when hovering over the card
-        card.onmouseover = (event) => {
-            showSongCardElements(['hover'], songId);
-        }
-        // Hide the song card hover elements when not hovering over the card
-        card.onmouseout = (event) => {
-            hideSongCardElements(['hover'], songId);
-        }
-    });
-}
-
-
-/*
- * Show the song card elements
- * @param {Array} elementNameList - List of element names to show
- * @param {string} songId - The ID of the song
- */
-function showSongCardElements(elementNameList, songId) {
-    elementNameList.forEach(name => {
-        const elementId = `song-${name}-${songId}`;
-        document.getElementById(elementId).classList.remove('hidden');
-    })
-}
-
-
-/*
- * Hide the song card elements
- * @param {Array} elementNameList - List of element names to hide
- * @param {string} songId - The ID of the song
- */
-function hideSongCardElements(elementNameList, songId) {
-    elementNameList.forEach(name => {
-        const elementId = `song-${name}-${songId}`;
-        document.getElementById(elementId).classList.add('hidden');
-    })
-}
-
-
-/*
- * Handle the click event for the edit button on a song card
- * @param {HTMLElement} element - The edit button element
- */
-function songEditButtonClick(element) {
-    const songId = element.dataset.songId;
-
-    // swap the visible elements of the card
-    hideSongCardElements(['text', 'edit', 'delete'], songId);
-    showSongCardElements(['input', 'save', 'cancel'], songId);
-
-    // set focus to the input control
-    document.getElementById(`song-input-${songId}`).focus();
-    document.getElementById(`song-input-${songId}`).select();
-
-    // event handler for clicking the save button
-    document.getElementById(`song-save-${songId}`).onclick = (event) => {
-
-        // get the new song name that was entered
-        const inputField = `song-input-${songId}`;
-        const songName = document.getElementById(inputField).value;
-
-        // call the API to update the song name on the backend
-        if (apiSongEdit(songId, songName)) {
-            // update the text on the song card
-            document.getElementById(`song-text-${songId}`).innerHTML = songName;
-
-            // swap the visible elements of the card back to the default
-            hideSongCardElements(['input', 'save', 'cancel'], songId);
-            showSongCardElements(['text', 'edit', 'delete'], songId);
-        } else {
-            // API call returned and error, do nothing
-            console.log('apiSongEdit() returned an error, check the log.')
-        }
-    }
-
-    // event handler for clicking the cancel button
-    document.getElementById(`song-cancel-${songId}`).onclick = (event) => {
-
-        // swap the visible elements of the card back to the default
-        hideSongCardElements(['input', 'save', 'cancel'], songId);
-        showSongCardElements(['text', 'edit', 'delete'], songId);
-    }
-}
-
-
-/*
- * Handle the click event for the delete button on a song card
- * @param {HTMLElement} element - The delete button element
- */
-function songDeleteButtonClick(element) {
-    document.getElementById('modal-delete-yes').onclick = (event) => {
-        if (apiSongDelete(element.dataset.songId)) {
-            const card = element.parentElement.parentElement;
-            const container = card.parentElement;
-            container.removeChild(card);
-        }
-    }
-    const card = element.parentElement.parentElement;
-    document.getElementById('modal-delete-message').innerHTML = `Are you sure you want to delete the song: '${card.dataset.songName}'?`;
-    document.getElementById('modal-delete').showModal();
-}
