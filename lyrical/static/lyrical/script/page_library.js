45e3692d98c855b8dd464c2ba573e18fadd906b5
Michael Petrou <mpetrou74@hotmail.com>
2025-05-25 19:05:57 +0930

    Refactor panel layout: update IDs for consistency, enhance toast system initialization, and add horizontal resizing functionality.

--- lyrical/static/lyrical/script/page_library.js
+++ lyrical/static/lyrical/script/page_library.js
@@ -1,36 +1,59 @@
 import { makeVerticallyResizable } from './util_sliders_vertical.js'
+import { makeHorizontallyResizable } from './util_sliders_horizontal.js'
+import { ToastSystem } from './util_toast.js';
+
+
+/**
+ * Declare the toast system at the module level
+ */
+let toastSystem;
 
 
 /**
  * Initialize the page when the DOM is fully loaded
  */
 document.addEventListener('DOMContentLoaded', () => {
-    // setup the resize elements of the page
+    // Setup the resize elements of the page
     setupResizeElements();
 
-    // bind the edit song name button
-    document.getElementById('btn-edit-song-name').onclick = editSongName;
+    // Initialize the toast system
+    initToastSystem();
 });
 
 
 /**
- * Setup the vertical resizable elements on the page.
+ * Initialize the toast system.
+ * Creates and configures the toast system for displaying user feedback messages.
  */
-function setupResizeElements() {
-    makeVerticallyResizable(
-        document.getElementById('panel1-top-content'),
-        document.getElementById('panel1-vertical-splitter'),
-        document.getElementById('panel1-bottom-content')
-    );
+function initToastSystem() {
+    // create the toast system and assign to module-level variable
+    toastSystem = new ToastSystem();
+
+    // initialize the toast system
+    toastSystem.init();
 }
 
 
 /**
- * Edit song name (placeholder function).
- * @param {Event} event - The click event that triggered the function.
+ * Setup the horizontal and vertical resizable elements on the page.
  */
-function editSongName(event) {
-    alert("clicked!")
+function setupResizeElements() {    
+    // Make the panel header resizable with auto-sizing to fit bottom content
+    makeVerticallyResizable(
+        document.getElementById('panel-top-content'),
+        document.getElementById('panel-vertical-splitter'),
+        document.getElementById('panel-bottom-content'),
+        { autoSizeToFitBottomContent: true }
+    );
+    // Make the first panel splitter resizable
+    makeHorizontallyResizable(
+        document.getElementById('panel2'), 
+        document.getElementById('splitter1'), 
+        document.getElementById('panel1'));
+    // Make the second panel splitter resizable
+    makeHorizontallyResizable(
+        document.getElementById('panel3'), 
+        document.getElementById('splitter2'), 
+        document.getElementById('panel2'));
 }
-
 
