77f5e5a9501c0b5c39c32dca5c861d5c03b8c0a3
Michael Petrou <mpetrou74@hotmail.com>
2025-05-28 13:02:54 +0930

    Refactor song selection logic: integrate SelectSystem for improved element selection and styling, enhance filtering functionality, and add event handlers for selection changes.

--- lyrical/static/lyrical/script/page_song.js
+++ lyrical/static/lyrical/script/page_song.js
@@ -1,10 +1,78 @@
 
-/**
- * Initialize the page when the DOM is fully loaded
- */
+import { SelectSystem } from './util_select.js';
+
+
+let selectSystem;
+
+
 document.addEventListener('DOMContentLoaded', () => {
     applyFilter();
     createEventHandlers();
+
+    // create the select system and assign to module-level variable
+    selectSystem = new SelectSystem();
+
+    // initialise the select system
+    selectSystem.init(
+        {
+            allowMultiSelect: false,
+            allowSelectOnClick: true,
+            allowDeselectOnClick: false,
+            allowNoSelection: true,
+            autoSelectFirstElement: false,
+            canDeselectOnEscape: true,
+            canDeselectOnClickAway: false,
+            selectOnMouseDown: true
+        },
+        {
+            onElementSelected: (element, allSelectedElements) => {
+                applySelectionStyles(element);
+                updateButtonStylesForSelection(element);
+            },
+            onElementDeselected: (element, allSelectedElements) => {
+                removeSelectionStyles(element);
+            },
+            onAfterElementChanged: (allSelectedElements, changedElement) => {
+                // if no elements are selected, update the button styles to disabled
+                const selectedElement = selectSystem.getSelectedElement();
+                if (selectedElement === null) {
+                    updateButtonStylesForSelection(null);
+                }
+            }
+        }
+    );
+
+    // begin with all buttons disabled
+    updateButtonStylesForSelection(null);
+
+    // register existing song cards with the select system
+    document.querySelectorAll('.song-card').forEach(card => {
+        selectSystem.addElement(card);
+    });
+
+    // register click away elements
+    // selectSystem.addClickAwayElement(document.getElementById('new-songs-container'));
+
+    // add event listener for Enter key press when no input is focused to
+    // edit the selected song
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            if (document.activeElement === document.body) {
+                if (selectSystem.hasSelectedElement()) {
+
+// next button -->
+
+                }
+            }
+        }
+    });
+
+
+
+// ---> select first *visible* element
+
+
+
 });
 
 
@@ -31,11 +99,13 @@ function applyFilter() {
     const filterGenerated = document.getElementById('filter-generated');
     const filterPublished = document.getElementById('filter-published');
 
-    // get the song list
+    // get the song list and the search term
     const songList = document.getElementById('song-list');
-
     const searchTerm = filterTerm.value.trim();
-    console.log(`search term: ${searchTerm}`)
+
+
+// ---> consider updating selection
+
 
     Array.from(songList.children).forEach(node => {
         if (searchTerm === '' || node.dataset.songName.toLowerCase().includes(searchTerm.toLowerCase())) {
@@ -54,7 +124,27 @@ function applyFilter() {
             node.classList.add('hidden');
         }
     });
+}
 
 
+function applySelectionStyles(element) {
+    const selectionStyleToAdd = ['border-2', 'border-primary'];
+    const selectionStyleToRemove = ['border-base-300'];
+    
+    element.classList.add(...selectionStyleToAdd);
+    element.classList.remove(...selectionStyleToRemove);
+}
+
+
+function removeSelectionStyles(element) {
+    const selectionStyleToAdd = ['border-2', 'border-primary'];
+    const selectionStyleToRemove = ['border-base-300'];
+    
+    element.classList.add(...selectionStyleToRemove);
+    element.classList.remove(...selectionStyleToAdd);
+}
+
+
+function updateButtonStylesForSelection(element) {
 
 }
