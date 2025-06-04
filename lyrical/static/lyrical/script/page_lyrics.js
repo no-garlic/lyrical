5766d1271d5f4b05d2b1f9cd2dd8148c312d0edf
Michael Petrou <mpetrou74@hotmail.com>
2025-06-04 12:38:23 +0930

    feat: Enhance lyrics editing functionality with dirty state tracking and save history management

--- lyrical/static/lyrical/script/page_lyrics.js
+++ lyrical/static/lyrical/script/page_lyrics.js
@@ -3,11 +3,16 @@ import { apiLyricsEdit } from './api_lyrics_edit.js';
 import { toastSystem } from './util_toast.js';
 
 
+let lyricsDirty = false;
+let lyricsHistory = {}
+
+
 const songId = document.body.dataset.songId;
 
 
 document.addEventListener('DOMContentLoaded', () => {
     initPageActions();
+    copyToSaveHistory();
 });
 
 
@@ -20,9 +25,33 @@ function initPageActions() {
 
     document.getElementById('btn-navigate-next').innerHTML = '<p>Export Song</p>';
 
+    document.querySelectorAll('[id*="lyrics-text-"').forEach(item => {
+        item.addEventListener('input', setLyricsDirty);
+    });
+
     const saveButton = document.getElementById('btn-save');
-    saveButton.classList.remove('btn-disabled');
     saveButton.onclick = saveLyrics;
+    const cancelButton = document.getElementById('btn-cancel');
+    cancelButton.onclick = cancelLyrics;
+}
+
+
+function setLyricsDirty(dirty = true) {
+    if (dirty && !lyricsDirty) {
+        lyricsDirty = true;
+
+        const saveButton = document.getElementById('btn-save');
+        saveButton.classList.remove('btn-disabled');
+        const cancelButton = document.getElementById('btn-cancel');
+        cancelButton.classList.remove('btn-disabled');
+    } else if (!dirty && lyricsDirty) {
+        lyricsDirty = false;
+
+        const saveButton = document.getElementById('btn-save');
+        saveButton.classList.add('btn-disabled');
+        const cancelButton = document.getElementById('btn-cancel');
+        cancelButton.classList.add('btn-disabled');
+    }
 }
 
 
@@ -39,23 +68,41 @@ function saveLyrics() {
     apiLyricsEdit(songId, lyrics)
         .then(songId => {
             console.log(`sucessfully saved song lyrics for song id=${songId}`)
+            copyToSaveHistory();
+            setLyricsDirty(false);
         })
         .catch(error => {
             // handle the error if the API call fails
             console.error('Failed to edit the song lyrics:', error);
             toastSystem.showError('Failed to update the song lyrics. Please try again.');
         });
-
 }
 
 
-function exportSong() {
-    
+function cancelLyrics() {
+    copyFromSaveHistory();
+    setLyricsDirty(false);
+}
+
+
+function copyToSaveHistory() {
+    document.querySelectorAll('[id*="lyrics-text-"').forEach(item => {
+        lyricsHistory[item.dataset.lyricsId] = item.value;
+    });
+}
+
+
+function copyFromSaveHistory() {
+    document.querySelectorAll('[id*="lyrics-text-"').forEach(item => {
+        item.value = lyricsHistory[item.dataset.lyricsId];
+    });
+}
+
+
+function exportSong() {    
 }
 
 
 function navigatePrevious() {
-    const generateButton = document.getElementById('btn-generate');
-    const songId = parseInt(generateButton.dataset.songId);
     window.location.href = `/hook/${songId}`;
 }
